2bcfc277390dad8c5c52a2355a7b19b8cc6572dd
guinaka <guinaka2006@gmail.com>
2025-06-27 19:31:59 -0300

    Peças
    Códigos de todas as peças do jogo de xadrez

--- src/xadrez/Dama.java
+++ src/xadrez/Dama.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package xadrez;
 
-/**
- *
- * @author ggmasi
- */
-public class Dama {
-    
+public class Dama extends Peca {
+
+    public Dama(String cor) {
+        super(cor);
+        this.representacao = 'Q';
+    }
+
+    @Override
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) {
+        int difLinha = Math.abs(linhaO - linhaD);
+        int difColuna = Math.abs(colunaO - colunaD);
+        return (linhaO == linhaD || colunaO == colunaD || difLinha == difColuna);
+    }
+
+    @Override
+    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD) {
+        if (!movimentoValido(linhaO, colunaO, linhaD, colunaD)) {
+            return "";
+        }
+
+        String caminho = "" + linhaO + colunaO;
+
+        int passoLinha = 0;
+        int passoColuna = 0;
+
+        if (linhaD > linhaO) passoLinha = 1;
+        else if (linhaD < linhaO) passoLinha = -1;
+
+        if (colunaD > colunaO) passoColuna = 1;
+        else if (colunaD < colunaO) passoColuna = -1;
+
+        int linha = linhaO + passoLinha;
+        char coluna = (char)(colunaO + passoColuna);
+
+        while (linha != linhaD || coluna != colunaD) {
+            caminho += "" + linha + coluna;
+            linha += passoLinha;
+            coluna += passoColuna;
+        }
+
+        caminho += "" + linhaD + colunaD;
+        return caminho;
+    }
+
 }
--- src/xadrez/Torre.java
+++ src/xadrez/Torre.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package xadrez;
 
-/**
- *
- * @author ggmasi
- */
-public class Torre {
-    
+public class Torre extends Peca {
+
+    public Torre(String cor) {
+        super(cor);
+        this.representacao = 'R';
+    }
+
+    @Override
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) {
+        return (linhaO == linhaD || colunaO == colunaD);
+    }
+
+    @Override
+    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD) {
+        if (!movimentoValido(linhaO, colunaO, linhaD, colunaD)) {
+            return "";
+        }
+
+        String caminho = "" + linhaO + colunaO;
+
+        int passoLinha = 0;
+        int passoColuna = 0;
+
+        if (linhaD > linhaO) passoLinha = 1;
+        else if (linhaD < linhaO) passoLinha = -1;
+
+        if (colunaD > colunaO) passoColuna = 1;
+        else if (colunaD < colunaO) passoColuna = -1;
+
+        int linha = linhaO + passoLinha;
+        char coluna = (char)(colunaO + passoColuna);
+
+        while (linha != linhaD || coluna != colunaD) {
+            caminho += "" + linha + coluna;
+            linha += passoLinha;
+            coluna += passoColuna;
+        }
+
+        caminho += "" + linhaD + colunaD;
+        return caminho;
+    }
 }
